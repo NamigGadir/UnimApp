d9a3aac9e5a66a74ee3801387f0d8bff1c7f2f7f
Mehdizade <kmehdiz@cmtclm2fj3e8md6.rd.corpintra.net>
2021-12-29 22:45:47 +0100

    feature/add_notification_tablayout: implemented Notfication tabLayout

--- app/src/main/java/com/unimapp/unimapp/ui/main/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/unimapp/unimapp/ui/main/notifications/NotificationsFragment.kt
@@ -1,21 +1,47 @@
 package com.unimapp.unimapp.ui.main.notifications
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.unimapp.unimapp.R
+import com.unimapp.unimapp.core.BaseFragment
+import com.unimapp.unimapp.databinding.FragmentNotificationsBinding
+import com.unimapp.unimapp.ui.main.notifications.activity.ActivityFragment
+import com.unimapp.unimapp.ui.main.notifications.opportunity.OpportunityFragment
+import com.unimapp.unimapp.ui.main.notifications.peers.PeersFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class NotificationsFragment : Fragment() {
+@AndroidEntryPoint
+class NotificationsFragment :
+    BaseFragment<NotificationsViewModel, FragmentNotificationsBinding, NotificationState, Unit>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    private val notificationViewPagerList by lazy {
+        listOf(ActivityFragment(), PeersFragment(), OpportunityFragment())
+    }
+    private val adapter by lazy {
+        NotificationsAdapter(parentFragmentManager, lifecycle, notificationViewPagerList)
     }
 
+    private val tabLayoutNames by lazy {
+        listOf(
+            getString(R.string.notification_activity),
+            getString(R.string.notification_peers),
+            getString(R.string.notification_opportunity)
+        )
+    }
 
+    override fun getViewModelClass() = NotificationsViewModel::class.java
+
+    override val onViewBinding: (LayoutInflater, ViewGroup?, Boolean) -> FragmentNotificationsBinding
+        get() = FragmentNotificationsBinding::inflate
+
+    override val onViewInit: FragmentNotificationsBinding.() -> Unit = {
+        notificationViewPage.adapter = adapter
+        TabLayoutMediator(
+            notificationTabLayout,
+            notificationViewPage,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                tab.text = tabLayoutNames[position]
+            }).attach()
+    }
 }
